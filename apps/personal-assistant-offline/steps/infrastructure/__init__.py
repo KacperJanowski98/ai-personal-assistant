95606498bbcdfbeb3ed78d661629cd3c9182d698
KacperJanowski98 <Janowski.Kacper1995@wp.pl>
2025-04-27 19:31:46 +0200

    feat: added fetch from mongodb step

--- apps/personal-assistant-offline/steps/infrastructure/__init__.py
+++ apps/personal-assistant-offline/steps/infrastructure/__init__.py
@@ -1,8 +1,10 @@
+from .fetch_from_mongodb import fetch_from_mongodb
 from .ingest_to_mongodb import ingest_to_mongodb
 from .read_documents_from_disk import read_documents_from_disk
 from .save_documents_to_disk import save_documents_to_disk
 
 __all__ = [
+    "fetch_from_mongodb",
     "ingest_to_mongodb",
     "read_documents_from_disk",
     "save_documents_to_disk",
