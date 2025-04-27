2f88a855efdba82448142a3091159b5af89f8c95
KacperJanowski98 <Janowski.Kacper1995@wp.pl>
2025-04-27 20:33:27 +0200

    feat: added save dataset to disk step

--- apps/personal-assistant-offline/steps/infrastructure/__init__.py
+++ apps/personal-assistant-offline/steps/infrastructure/__init__.py
@@ -1,6 +1,7 @@
 from .fetch_from_mongodb import fetch_from_mongodb
 from .ingest_to_mongodb import ingest_to_mongodb
 from .read_documents_from_disk import read_documents_from_disk
+from .save_dataset_to_disk import save_dataset_to_disk
 from .save_documents_to_disk import save_documents_to_disk
 
 __all__ = [
@@ -8,4 +9,5 @@ __all__ = [
     "ingest_to_mongodb",
     "read_documents_from_disk",
     "save_documents_to_disk",
+    "save_dataset_to_disk",
 ]
