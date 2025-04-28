60fecebbfb293c0787575a4e991ea9989048cf69
KacperJanowski98 <Janowski.Kacper1995@wp.pl>
2025-04-28 19:28:07 +0200

    fix: added missing init config in infrastructure step

--- apps/personal-assistant-offline/steps/infrastructure/__init__.py
+++ apps/personal-assistant-offline/steps/infrastructure/__init__.py
@@ -1,5 +1,6 @@
 from .fetch_from_mongodb import fetch_from_mongodb
 from .ingest_to_mongodb import ingest_to_mongodb
+from .push_to_huggingface import push_to_huggingface
 from .read_documents_from_disk import read_documents_from_disk
 from .save_dataset_to_disk import save_dataset_to_disk
 from .save_documents_to_disk import save_documents_to_disk
@@ -7,6 +8,7 @@ from .save_documents_to_disk import save_documents_to_disk
 __all__ = [
     "fetch_from_mongodb",
     "ingest_to_mongodb",
+    "push_to_huggingface",
     "read_documents_from_disk",
     "save_documents_to_disk",
     "save_dataset_to_disk",
