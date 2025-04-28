9f5f480f45b6919c9a12ac1411bc5e49decf15ff
KacperJanowski98 <Janowski.Kacper1995@wp.pl>
2025-04-28 19:16:12 +0200

    feat: added generate dataset pipeline

--- apps/personal-assistant-offline/pipelines/__init__.py
+++ apps/personal-assistant-offline/pipelines/__init__.py
@@ -1,7 +1,9 @@
 from .collect_notion_data import collect_notion_data
 from .etl import etl
+from .generate_dataset import generate_dataset
 
 __all__ = [
     "collect_notion_data",
     "etl",
+    "generate_dataset"
 ]
