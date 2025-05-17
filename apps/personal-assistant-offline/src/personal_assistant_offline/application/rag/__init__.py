344aebfab08936c9d681027837513e0015a44e9e
KacperJanowski98 <Janowski.Kacper1995@wp.pl>
2025-05-17 19:23:19 +0200

    feat: added retrievers for rag

--- apps/personal-assistant-offline/src/personal_assistant_offline/application/rag/__init__.py
+++ apps/personal-assistant-offline/src/personal_assistant_offline/application/rag/__init__.py
@@ -1,10 +1,12 @@
 from .embeddings import EmbeddingModelType, get_embedding_model
+from .retrievers import RetrieverType, get_retriever
 from .splitters import SummarizationType, get_splitter
 
-
 __all__ = [
+    "get_retriever",
     "get_splitter",
-    "SummarizationType",
     "EmbeddingModelType",
     "get_embedding_model",
+    "RetrieverType",
+    "SummarizationType",
 ]
