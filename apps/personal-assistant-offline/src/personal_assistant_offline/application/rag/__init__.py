c92fade3270bc7c5a39c2edf864b5a9794116fd4
KacperJanowski98 <Janowski.Kacper1995@wp.pl>
2025-05-17 19:16:02 +0200

    feat: added embedding for rag

--- apps/personal-assistant-offline/src/personal_assistant_offline/application/rag/__init__.py
+++ apps/personal-assistant-offline/src/personal_assistant_offline/application/rag/__init__.py
@@ -1,7 +1,10 @@
+from .embeddings import EmbeddingModelType, get_embedding_model
 from .splitters import SummarizationType, get_splitter
 
 
 __all__ = [
     "get_splitter",
     "SummarizationType",
+    "EmbeddingModelType",
+    "get_embedding_model",
 ]
