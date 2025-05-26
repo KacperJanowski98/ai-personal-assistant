45ee7fbbc670d2987afb017b1fa572e9a2bf18c8
KacperJanowski98 <Janowski.Kacper1995@wp.pl>
2025-05-26 21:18:02 +0200

    feat: added spliter for rag

--- apps/personal-assistant-online/src/personal_assistant_online/application/rag/__init__.py
+++ apps/personal-assistant-online/src/personal_assistant_online/application/rag/__init__.py
@@ -1,6 +1,8 @@
 from .embeddings import EmbeddingModelType, get_embedding_model
+from .splitters import get_splitter
 
 __all__ = [
     "EmbeddingModelType",
     "get_embedding_model",
+    "get_splitter",
 ]
